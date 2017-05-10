0d3b9903c914bdb9f015a3fdf2af7d7185d43fe2
MidXORFeed <ngowi@sheridancollege.ca>
2017-05-09 21:13:02 -0400

    - Loading screen in progress

---
+++ Assets/Scripts/LoadingScreenManager/LoadTargetSceneButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadTargetSceneButton : MonoBehaviour {
+
+    public void LoadSceneNum(int num)
+    {
+        if (num < 0 || num >= SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("Cant load scene num " + num + "SceneManager only has " + SceneManager.sceneCountInBuildSettings + " scenes in BuildSettings!");
+            return;
+        }
+        LoadingScreenManager.LoadScene(num);
+    }
+}
