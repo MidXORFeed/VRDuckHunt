827065cb7cf29bdb1a4974d81347a71b9413a14c
Wilton Ngo <wiltonngo@gmail.com>
2017-05-16 04:21:03 -0400

    - Fixed an issue causing multiple audio listeners to presist while trying to get to loading scene - Created S&W .45ACP prefab placeholder for gun - Created .45ACP magazine prefab - Created .45ACP bullet prefab - S&W can now be picked up

---
+++ Assets/Pistol.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Pistol : VRTK_InteractableObject
+{
+    protected void Start()
+    {
+
+    }
+
+    protected override void Update()
+    {
+
+    }
+}
