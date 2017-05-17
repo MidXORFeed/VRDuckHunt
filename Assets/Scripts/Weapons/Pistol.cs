3fbd0d45a4900750730fbdfb823d10cff77786bf
Wilton Ngo <wiltonngo@gmail.com>
2017-05-17 13:43:59 -0400

    - Pistol Slider now working - Haptic feedback when trigger is pressed - Some scene modifications

--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -2,16 +2,123 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
+using VRTK.Examples;
 
 public class Pistol : VRTK_InteractableObject
 {
-    protected void Start()
-    {
+    public float bulletSpeed = 200f;
+    public float bulletLife = 5f;
 
+    public GameObject bullet;
+    public GameObject trigger;
+    public PistolSlide slide;
+
+    private Rigidbody slideRigidbody;
+    private Collider slideCollider;
+
+    private VRTK_ControllerEvents controllerEvents;
+
+    private float minTriggerRotation = -10f;
+    private float maxTriggerRotation = 45f;
+
+    private void ToggleCollision(Rigidbody objRB, Collider objCol, bool state)
+    {
+        objRB.isKinematic = state;
+        objCol.isTrigger = state;
+    }
+
+    private void ToggleSlide(bool state)
+    {
+        if (!state)
+        {
+            slide.ForceStopInteracting();
+        }
+        slide.enabled = state;
+        slide.isGrabbable = state;
+        ToggleCollision(slideRigidbody, slideCollider, state);
+    }
+
+    public override void Grabbed(GameObject currentGrabbingObject)
+    {
+        base.Grabbed(currentGrabbingObject);
+
+        controllerEvents = currentGrabbingObject.GetComponent<VRTK_ControllerEvents>();
+
+        ToggleSlide(true);
+
+        //Limit hands grabbing when picked up
+        if (VRTK_DeviceFinder.GetControllerHand(currentGrabbingObject) == SDK_BaseController.ControllerHand.Left)
+        {
+            allowedTouchControllers = AllowedController.LeftOnly;
+            allowedUseControllers = AllowedController.LeftOnly;
+            slide.allowedGrabControllers = AllowedController.RightOnly;
+        }
+        else if (VRTK_DeviceFinder.GetControllerHand(currentGrabbingObject) == SDK_BaseController.ControllerHand.Right)
+        {
+            allowedTouchControllers = AllowedController.RightOnly;
+            allowedUseControllers = AllowedController.RightOnly;
+            slide.allowedGrabControllers = AllowedController.LeftOnly;
+        }
+    }
+
+    public override void Ungrabbed(GameObject previousGrabbingObject)
+    {
+        base.Ungrabbed(previousGrabbingObject);
+
+        ToggleSlide(false);
+
+        //Unlimit hands
+        allowedTouchControllers = AllowedController.Both;
+        allowedUseControllers = AllowedController.Both;
+        slide.allowedGrabControllers = AllowedController.Both;
+
+        controllerEvents = null;
+    }
+
+    public override void StartUsing(GameObject currentUsingObject)
+    {
+        base.StartUsing(currentUsingObject);
+        StartCoroutine(slide.Fire());
+        FireBullet();
+        VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(controllerEvents.gameObject), 0.63f, 0.2f, 0.01f);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (bullet)
+        {
+            bullet.SetActive(false);
+        }
+
+        if (slide)
+        {
+            slideRigidbody = slide.GetComponent<Rigidbody>();
+            slideCollider = slide.GetComponent<Collider>();
+        }
     }
 
     protected override void Update()
     {
+        base.Update();
+        if (controllerEvents)
+        {
+            var pressure = (maxTriggerRotation * controllerEvents.GetTriggerAxis()) - minTriggerRotation;
+            trigger.transform.localEulerAngles = new Vector3(0f, pressure, 0f);
+        }
+        else
+        {
+            trigger.transform.localEulerAngles = new Vector3(0f, minTriggerRotation, 0f);
+        }
+    }
 
+    private void FireBullet()
+    {
+        GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
+        bulletClone.SetActive(true);
+        Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
+        rb.AddForce(bullet.transform.forward * bulletSpeed);
+        Destroy(bulletClone, bulletLife);
     }
 }
---
+++ Assets/Scripts/Weapons/PistolSlide.cs
@@ -0,0 +1,80 @@
+﻿namespace VRTK.Examples
+{
+    using System.Collections;
+    using UnityEngine;
+
+    public class PistolSlide : VRTK_InteractableObject
+    {
+        public Transform RestTransform;
+        public Transform MaxSlideTransform;
+
+        private IEnumerator currentCoroutine;
+        private float SlideSpeed = 0.0005f;
+        private float SlideTime = 0.1f;
+
+        public IEnumerator Fire()
+        {
+            currentCoroutine = SlideBack();
+            StartCoroutine(currentCoroutine);
+
+            yield return new WaitForSeconds(SlideTime);
+
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+            }
+
+            currentCoroutine = SlideForth();
+            StartCoroutine(currentCoroutine);
+        }
+
+        IEnumerator SlideBack()
+        {
+            while (transform.localPosition != MaxSlideTransform.localPosition)
+            {
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, MaxSlideTransform.localPosition, SlideSpeed);
+                yield return null;
+            }
+        }
+
+        IEnumerator SlideForth()
+        {
+            while (transform.localPosition != RestTransform.localPosition)
+            {
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, RestTransform.localPosition, SlideSpeed);
+                yield return null;
+            }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            //base.Update();
+            //if (transform.localPosition.x <= restPosition)
+            //{
+            //    transform.localPosition = new Vector3(restPosition, transform.localPosition.y, transform.localPosition.z);
+            //}
+
+            //if (fireTimer == 0 && transform.localPosition.x > restPosition && !IsGrabbed())
+            //{
+            //    transform.localPosition = new Vector3(transform.localPosition.x - boltSpeed, transform.localPosition.y, transform.localPosition.z);
+            //}
+
+            //if (fireTimer > 0)
+            //{
+            //    transform.localPosition = new Vector3(transform.localPosition.x - boltSpeed, transform.localPosition.y, transform.localPosition.z);
+            //    fireTimer -= boltSpeed;
+            //}
+
+            //if (fireTimer < 0)
+            //{
+            //    fireTimer = 0;
+            //}
+        }
+    }
+}
