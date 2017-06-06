9a140f6e8f195df5faf640d84f6f54ef6faf3e0d
Wilton Ngo <wiltonngo@gmail.com>
2017-06-06 08:23:30 -0400

    - Added GameState Class - Added GameState Manager

---
+++ Assets/Scripts/GameState/GameStateManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+
+    Stack<GameState> gameStateStack;
+    GameState currentState;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
