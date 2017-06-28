6a5638f05ac5fdf5a1f05cc19e69d89af8e812ee
Chave <chave@squiggledome.com>
2017-06-28 16:04:09 -0400

    Implement Basic Enemy

---
+++ Assets/Scripts/Chave/SimpleEnemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : MonoBehaviour
+{
+    Transform target;
+    Animator enemy;
+    UnityEngine.AI.NavMeshAgent agent;
+    public int enemyMaxHeath = 3;
+    int enemyHeath;
+
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        enemy = GetComponentInChildren<Animator>();
+
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        enemyHeath = Random.Range(1, enemyMaxHeath);
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+                
+            if (enemyHeath != 0)
+            {
+                enemyHeath--;
+                enemy.Play("EnemyFlash");
+
+                if (enemyHeath == 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
